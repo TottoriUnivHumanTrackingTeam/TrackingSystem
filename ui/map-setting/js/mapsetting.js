3a69ef579e5765bf30f7ca43dd0e12517c29f671
dani0678 <B16T2070C@edu.tottori-u.ac.jp>
2019-06-25 13:16:44 +0900

    hairetu

--- ui/map-setting/js/mapsetting.js
+++ ui/map-setting/js/mapsetting.js
@@ -2,6 +2,7 @@ let bg;
 let radius = 8;
 let maps = [];
 let originalMaps = [];
+let maps_hozon = [];
 let width = 1020;
 let height = 645;
 
@@ -34,23 +35,26 @@ function draw() {
     clear();
     if(bg) image(bg, 0, 0, width, height);
     if (maps.length > 0) {
-        for (let map of maps) {
-			fill(0, 50);
-			quad(map.size.min.x, map.size.min.y, map.size.max.x, map.size.min.y, 
-				map.size.max.x, map.size.max.y, map.size.min.x, map.size.max.y);
+		for(let map of maps) {
+			for(i = 0; map.size.length > i; i++ ){
+				fill(0, 50);
+				quad(map.size[i].min.x, map.size[i].min.y, map.size[i].max.x, map.size[i].min.y, 
+					map.size[i].max.x, map.size[i].max.y, map.size[i].min.x, map.size[i].max.y);
+		
             if(map.active) {
-            textSize(10);
-			text(`mapName: ${map.mapName} `, map.size.min.x+radius+5, map.size.min.y);
-            fill('#000000');
+            	textSize(10);
+				text(`mapName: ${map.mapName} `, map.size.min.x+radius+5, map.size.min.y);
+            	fill('#000000');
             }
-        }
-    }
+        	}
+    	}
+	}
 }
 
 function mousePressed() {
 	let y = mouseY;
 	if(y < height){
-		maps.push({ name: 'new', size: {min: {x: mouseX, y: mouseY}, max: {x: mouseX, y: mouseY}}, color:'#ff8c00', active: false });
+		maps.push({ name: 'new', size: [{min: {x: mouseX, y: mouseY}, max: {x: mouseX, y: mouseY}}], color:'#ff8c00', active: false });
 		$('[name="name"]').val([map.name]);
 		return false;
 	}
@@ -58,13 +62,14 @@ function mousePressed() {
 
 function mouseReleased(){
 	let map = maps.find(map => map.name === 'new');
-	
+	maps_hozon = map;
+	console.log(maps_hozon);
 	if(map.name === 'new'){
 		$('[name="name"]').prop('disabled', false);
 	}else{
 		$('[name="name"]').prop('disabled', true); 
 	}
-    return false;
+	return false;
 }
 
 function doubleClicked() {
@@ -93,25 +98,22 @@ function mouseDragged() {
 	let y = mouseY;
 	if(y < height){
 	let map = maps.find(map => map.name === 'new');
-	map.size.max = { x: mouseX, y: mouseY };
+	map.size[0].max = { x: mouseX, y: mouseY };
 	}
 }
 
 const mapSubmit = function mapSubmit() {
 	let map1 = maps.find(map => map.name === 'new');
 	const mapName = $('[name="name"]').val();
-	const mapSize_min_x = map1.size.min.x;
-	const mapSize_min_y = map1.size.min.y;
-	const mapSize_max_x = map1.size.max.x;
-	const mapSize_max_y = map1.size.max.y;
+	const mapSize = []
+	for(i = 0; map1.size.length > i; i++ ){
+		mapSize[i] = map1.size[i] 
+	}
 	let map = maps.find(map => map.name === mapName);
 	if(!map) {
 		map = maps.find(map => map.name === 'new');
 		map.name = mapName;
-		map.size.min.x = Number(mapSize_min_x);
-		map.size.min.y = Number(mapSize_min_y);
-		map.size.max.x = Number(mapSize_max_x);
-		map.size.max.y = Number(mapSize_max_y);
+		map.size = mapSize;
 
 		$.ajax({
 			url:'http://localhost:3000/api/map/',
