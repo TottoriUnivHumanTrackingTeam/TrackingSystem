d5426a9c50e0991b1101b61e359c3a8807f0796a
dani0678 <B16T2070C@edu.tottori-u.ac.jp>
2019-07-16 16:33:51 +0900

    naosi

--- Alart/Alart.js
+++ Alart/Alart.js
@@ -18,7 +18,7 @@ module.exports = class Alart {
             const keepOutResult = KeepOut.check(tracker);
 
             if (lostResult || keepOutResult) {
-                const message = lostResult + '</br>' + keepOutResult + '</br></br><a href="http://192.168.235.135:3000">Web‚ÅŠm”F‚·‚é</a>';
+                const message = lostResult + '</br>' + keepOutResult + '</br></br><a href="http://192.168.235.135:3000">Webã§ç¢ºèªã™ã‚‹</a>';
                 this.sendMail(tracker, message);
             }else{
                 TrackerRepository.updateTracker(tracker.trackerID, tracker);
