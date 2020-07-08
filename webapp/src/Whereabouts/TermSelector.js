6d4e3193d466bc8c1b4dc2aa8fd3bbebc3e708e3
kato sana <b17t2023k@edu.tottori-u.ac.jp>
2020-07-08 10:56:21 +0900

    日時選択機能の日付と時刻の分割

--- webapp/src/Whereabouts/TermSelector.js
+++ webapp/src/Whereabouts/TermSelector.js
@@ -3,9 +3,12 @@ import Grid from '@material-ui/core/Grid';
 import DateFnsUtils from '@date-io/date-fns';
 import jaLocale from 'date-fns/locale/ja';
 import 'date-fns';
-import { DateTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import { DatePicker, TimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 
 export default function MaterialUIPickers(props) {
+  const [selectDate, setSelectDate] = useState(new Date());
+  const [startTime, setStartTime] = useState(new Date());
+  const [endTime,setEndTime] = useState(new Date());
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
@@ -14,33 +17,70 @@ export default function MaterialUIPickers(props) {
       start: startDate.getTime(),
       end: endDate.getTime()
     };
+    console.log(term);
     props.onSend(term);
   };
+
+  useEffect(() => {
+    const selectStartDate = new Date();
+    selectStartDate.setFullYear(selectDate.getFullYear());
+    selectStartDate.setMonth(selectDate.getMonth());
+    selectStartDate.setDate(selectDate.getDate());
+    selectStartDate.setHours(startTime.getHours());
+    selectStartDate.setMinutes(startTime.getMinutes());
+    selectStartDate.setSeconds(startTime.getSeconds());
+    selectStartDate.setMilliseconds(startTime.getMilliseconds());
+    setStartDate(selectStartDate);
+  }, [selectDate, startTime]);
+
+  useEffect(() => {
+    const selectEndDate = new Date();
+    selectEndDate.setFullYear(selectDate.getFullYear());
+    selectEndDate.setMonth(selectDate.getMonth());
+    selectEndDate.setDate(selectDate.getDate());
+    selectEndDate.setHours(endTime.getHours());
+    selectEndDate.setMinutes(endTime.getMinutes());
+    selectEndDate.setSeconds(endTime.getSeconds());
+    selectEndDate.setMilliseconds(endTime.getMilliseconds());
+    setEndDate(selectEndDate);
+  }, [selectDate, endTime]);
 
   useEffect(send, [endDate]);
 
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils} locale={jaLocale}>
       <Grid container justify="space-around">
-        <DateTimePicker
+        <DatePicker
           variant="inline"
-          label="開始時間"
-          format="yyyy/MM/dd h:mm"
-          value={startDate}
+          label="日付"
+          format="yyyy/MM/dd"
+          value={selectDate}
           onChange={date => {
-            setStartDate(date);
-            setEndDate(date);
+            setSelectDate(date);
           }}
         />
       </Grid>
       <br />
       <Grid container justify="space-around">
-        <DateTimePicker
+        <TimePicker
+          variant="inline"
+          label="開始時間"
+          format="HH:mm"
+          value={startTime}
+          onChange={date => {
+            setStartTime(date);
+            setEndTime(date);
+          }}
+        />
+      </Grid>
+      <br />
+      <Grid container justify="space-around">
+        <TimePicker
           variant="inline"
           label="終了時間"
-          format="yyyy/MM/dd h:mm"
-          value={endDate}
-          onChange={date => setEndDate(date)}
+          format="HH:mm"
+          value={endTime}
+          onChange={date => setEndTime(date)}
         />
       </Grid>
     </MuiPickersUtilsProvider>
