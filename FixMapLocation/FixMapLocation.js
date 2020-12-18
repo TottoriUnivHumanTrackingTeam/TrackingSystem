e7c4d14d5d0d45080f87ad840dd8045b7ab875a1
kato sana <b17t2023k@edu.tottori-u.ac.jp>
2020-12-18 14:33:59 +0900

    FixMapLocation全般の細かい修正

--- FixMapLocation/FixMapLocation.js
+++ FixMapLocation/FixMapLocation.js
@@ -1,10 +1,11 @@
 "use strict";
 
 module.exports = class FixMapLocation {
-  constructor(beaconID, grid, map, time) {
+  constructor(beaconID, grid, map, time, alert) {
     this.beaconID = beaconID;
     this.grid = grid;
     this.map = map;
     this.locatedTime = time;
+    this.alert;
   }
 };
